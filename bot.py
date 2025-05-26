6d7d5e983ac4817425f2240b11615934cba6c336
shaxriyordasturchi <shaxriyorcholiyev951@gmail.com>
2025-05-26 09:55:56 +0500

    bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,11 @@
 from telegram import Bot
 from telegram.error import TelegramError
-from config import BOT_TOKEN, ADMIN_CHAT_ID
+from config import 7899690264:AAH14dhEGOlvRoc4CageMH6WYROMEE5NmkY, -1002671611327
 
-bot = Bot(token=BOT_TOKEN)
+bot = Bot(token=7899690264:AAH14dhEGOlvRoc4CageMH6WYROMEE5NmkY)
 
 def send_telegram_message(text):
     try:
-        bot.send_message(chat_id=ADMIN_CHAT_ID, text=text, parse_mode='HTML')
+        bot.send_message(chat_id=-1002671611327, text=text, parse_mode='HTML')
     except TelegramError as e:
         print("Telegram error:", e)
