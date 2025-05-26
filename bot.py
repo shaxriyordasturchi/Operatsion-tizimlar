3c97bc8e9d62b72554ec59e9256eccbce90d194d
shaxriyordasturchi <shaxriyorcholiyev951@gmail.com>
2025-05-26 10:03:30 +0500

    bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,17 @@
 from telegram import Bot
 from telegram.error import TelegramError
-from config import BOT_TOKEN, ADMIN_CHAT_ID
+import logging
 
-bot = Bot(token=BOT_TOKEN)
+# Telegram bot tokeningizni shu yerga yozing
+TELEGRAM_TOKEN = "7899690264:AAH14dhEGOlvRoc4CageMH6WYROMEE5NmkY"
+CHAT_ID = "-1002671611327"  # Sizga xabar yuboriladigan chat id (odatda adminning telegram idsi)
 
-def send_telegram_message(text):
+bot = Bot(token=TELEGRAM_TOKEN)
+
+def send_telegram_message(message: str):
     try:
-        bot.send_message(chat_id=ADMIN_CHAT_ID, text=text, parse_mode='HTML')
+        bot.send_message(chat_id=CHAT_ID, text=message, parse_mode="HTML")
+        print("✅ Xabar Telegramga yuborildi.")
     except TelegramError as e:
-        print("Telegram error:", e)
+        logging.error(f"Telegramga xabar yuborishda xatolik yuz berdi: {e}")
+        print("❌ Telegramga yuborishda muammo yuz berdi.")
