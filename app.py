aaa005feaf1156b8866f3f9b01da11430023a241
shaxriyordasturchi <shaxriyorcholiyev951@gmail.com>
2025-05-27 05:20:05 +0500

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from telegram import Bot
 
 # Telegram sozlamalari
 TELEGRAM_BOT_TOKEN = "7817066006:AAHRcf_wJO4Kmq5PvOrdq5BPi_eyv5vYqaM"
+CHAT_ID = 7750409176
 bot = Bot(token=TELEGRAM_BOT_TOKEN)
 
 DB_PATH = "worktime.db"
